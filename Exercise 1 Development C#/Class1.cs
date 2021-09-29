f673ccd260e796ae324c824418d32a5b32dca425
HeribertoVas <91218321+HeribertoVas@users.noreply.github.com>
2021-09-28 23:20:11 -0500

    Exercise 1 C# Uploaded

---
+++ Exercise 1 Development C#/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Development_C_
+{
+    class Class1
+    {
+
+        string StringComparison(string string1,string string2){
+            if(string1.Equals(string2)){
+                return "Both strings are equal";
+            }
+            return "Strings are not equal";
+        }
+        
+        void VerifyBiggestNumber(int[] numbers){
+            int max = numbers[0];
+            for (int i = 1; i < numbers.Length; i++){
+                if (numbers[i] > max){
+                    max = numbers[i];
+                }
+            }
+            
+            Console.WriteLine(max);
+        }
+        
+        void SortList(List<string> strs){
+            var words = strs;
+            words.Sort();
+            Console.WriteLine(string.Join(",",words));
+            words.Reverse();
+            Console.WriteLine(string.Join(",",words));
+        }
+
+        
+        int CountStringLength(string str){
+
+            return str.Length;
+        } 
+        
+        static void Main(string[] args)
+        {
+
+            Class1 obj1 = new Class1();
+            Console.WriteLine(obj1.StringComparison("abc","cb"));
+            obj1.VerifyBiggestNumber(new int[] {10,2,3,4,5,8,7});
+            obj1.SortList(new List<string> {"ghi","abc","def","abc"});
+            Console.WriteLine(obj1.CountStringLength("Beto"));
+
+        }
+    }
+
+    
+}
---
+++ Exercise 1 Development C#/Class2/Class2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Development_C_
+{
+    class Class2
+        {
+            void PrintFirstFiveValues(List<int> listI){
+                for (int i = 0; i < 5; i++){
+                    Console.WriteLine(listI[i]);
+                }
+            }
+            
+            void PrintSmallestNumber(int[] numbers){
+                int min = numbers[0];
+                for (int i = 1; i < numbers.Length; i++){
+                    if (numbers[i] < min){
+                        min = numbers[i];
+                    }
+                }
+                
+                Console.WriteLine(min);
+            }
+            
+            void PrintDictionaryByKey(){
+                Dictionary<string, string> dictionary =new Dictionary<string, string>();
+                dictionary.Add("txt", "notepad.exe");
+                dictionary.Add("bmp", "paint.exe");
+                dictionary.Add("dib", "paint.exe");
+                dictionary.Add("rtf", "wordpad.exe");
+
+                foreach(KeyValuePair<string, string> entry in dictionary){
+                    Console.WriteLine(entry.Value);
+                }
+            }
+
+            
+            static void Main(string[] args)
+            {
+
+                Class2 obj2 = new Class2();
+                obj2.PrintFirstFiveValues(new List<int>{1,2,3,4,5,6,7,8});
+                obj2.PrintSmallestNumber(new int[] {2,10,11,9,8});
+                obj2.PrintDictionaryByKey();
+
+            }
+        }
+}
