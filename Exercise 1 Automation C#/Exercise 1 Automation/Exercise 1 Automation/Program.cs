6ca18d9111b975313b385472c6b392b9ad35b05c
HeribertoVas <91218321+HeribertoVas@users.noreply.github.com>
2021-09-29 16:48:54 -0500

    Exercise 1 Automation C# Completed

--- Exercise 1 Automation C#/Exercise 1 Automation/Exercise 1 Automation/Program.cs
+++ Exercise 1 Automation C#/Exercise 1 Automation/Exercise 1 Automation/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +15,20 @@ namespace Exercise_1_Automation
     class Program
     {
         IWebDriver driver;
-        public IWebDriver SetUpDriver()
+        public IWebDriver SetUpDriver(string driverToSelect)
         {
-            //driver = new ChromeDriver(@"C:\Users\heriberto.vasquez\Documents\WebDrivers\Chrome"); //local webdriver
-            driver = new ChromeDriver();
+            switch (driverToSelect)
+            {
+                case "Chrome":
+                    driver = new ChromeDriver(@"C:\Users\heriberto.vasquez\Documents\WebDrivers\Chrome"); //local webdriver
+                    //driver = new ChromeDriver();
+                    break;
+                case "Mozilla":
+                    driver = new FirefoxDriver();
+                    break;
+
+            }
+            
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
             return driver;
@@ -31,7 +44,40 @@ namespace Exercise_1_Automation
             element.SendKeys(value);
         }
 
-        //region Facebook Locators
+        public void explicitWait(int seconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+        }
+
+        public void waitForElement(int seconds, By xpath)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            wait.Until(OpenQA.Selenium.Support.UI.ExpectedConditions.ElementExists(xpath));
+        }
+
+        public void switchTab(int tabNum)
+        {
+            driver.SwitchTo().Window(driver.WindowHandles[tabNum]);
+        }
+
+        public void waitElementUnexistent(int seconds, By xpath)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            try
+            {
+                IWebElement element = driver.FindElement(xpath);
+                Assert.IsTrue(element.Displayed);
+                wait.Until(OpenQA.Selenium.Support.UI.ExpectedConditions.ElementExists(xpath));
+                
+            }
+            catch
+            {
+                Console.WriteLine("There is no six Sense jet!");
+            }
+            
+        }
+
+        #region Facebook Locators
         By initialMessage = By.XPath("//h2[text()='Facebook te ayuda a comunicarte y compartir con las personas que forman parte de tu vida.']");
         By createAccount = By.XPath("//a[text()='Crear cuenta nueva']");
         By firstName = By.XPath("//input[@placeholder='Nombre']");
@@ -40,7 +86,7 @@ namespace Exercise_1_Automation
         By newPwd = By.XPath("//input[@name='reg_passwd__']");
         By sixSense = By.XPath("//a[text()=' 6. Sexto sentido ']");//not real
 
-        By termsC = By.XPath("//a[text()='Condiciones']");
+        By termsC = By.XPath("//a[text()='Condiciones' and @id='terms-link']");
         By titleTerms = By.XPath("//h2[text()='Condiciones del servicio']");
         By oneService = By.XPath("//a[text()=' 1. Los servicios que proporcionamos ']");
         By twoHow = By.XPath("//a[text()=' 2. Cómo se pagan nuestros servicios ']");
@@ -53,7 +99,7 @@ namespace Exercise_1_Automation
         By cookiesPolicy = By.XPath("//div[text()=' Política de cookies ']");
         By dataPolicy = By.XPath("//div[text()=' Política de datos ']");
         By moreResources = By.XPath("//div[text()=' Más recursos ']");
-        //endregion
+        #endregion
 
 
 
@@ -62,7 +108,7 @@ namespace Exercise_1_Automation
             IWebDriver Browser;
             IWebElement element;
             Program program = new Program();
-            Browser = program.SetUpDriver();
+            Browser = program.SetUpDriver("Chrome");
             Browser.Url = "https://www.facebook.com/";
 
             //review initial message
@@ -74,33 +120,31 @@ namespace Exercise_1_Automation
             program.Click(element);
 
             // fill information
-            element = Browser.FindElement(program.firstName);            
-            program.SendText(element,"Heriberto");
+            program.waitForElement(1, program.firstName);
+            element = Browser.FindElement(program.firstName);
+            program.SendText(element, "Heriberto");
 
+            program.waitForElement(1, program.lastName);
             element = Browser.FindElement(program.lastName);
             program.SendText(element, "Vasquez");
 
+            program.waitForElement(1, program.mobileNumber);
             element = Browser.FindElement(program.mobileNumber);
             program.SendText(element, "5564342426");
 
+            program.waitForElement(1, program.newPwd);
             element = Browser.FindElement(program.newPwd);
             program.SendText(element, "This is a password!");
 
-            //element inexistent / try catch handled
-            element = Browser.FindElement(program.sixSense);
-            try
-            {
-                Assert.IsTrue(element.Displayed);
-            }
-            catch
-            {
-                Console.WriteLine("There is no six Sense jet!");
-            }
+            //element inexistent 
+            program.waitElementUnexistent(1,program.sixSense);
             
             //2nd Test Case Go to terms
 
             element = Browser.FindElement(program.termsC);
             program.Click(element);
+
+            program.switchTab(1);//Switch to control new tab
 
             //Validate terms title exists
             element = Browser.FindElement(program.titleTerms);
@@ -132,8 +176,9 @@ namespace Exercise_1_Automation
 
             for(int i = 0; i< webElements.Count; i++)
             {
-                Console.WriteLine(webElements[i]);
+                Console.WriteLine(webElements[i].Text);
             }
+            program.explicitWait(10);
 
             Browser.Close();
             Browser.Quit();
